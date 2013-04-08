06360b9be3b95ef9908ebe6e49c35ba47b9576cb
Vadim Vorotilin <v.vorotilin@patrioticgames.org>
2013-04-08 21:03:20 +0300

    Moving function added. Loading from file fixed.

--- Test2/Test2/Program.cs
+++ Test2/Test2/Program.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test2 {
 	
 	class Program {
 
 		private const string InFilename = "in.txt";
-		private const string OutFilename = "out.txt";
-
-		private const char Separator = ' ';
 
 		static void Main () {
 
 			Sokoban sokoban = new Sokoban();
-			sokoban.Load(InFilename, Separator);
+			sokoban.Load(InFilename);
+
+			Console.ReadKey();
 
 		}
 
--- Test2/Test2/Sokoban.cs
+++ Test2/Test2/Sokoban.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace Test2 {
 
@@ -18,6 +17,30 @@ namespace Test2 {
 				this.y = y;
 			}
 
+			/// <summary>
+			/// Calculates moved point.
+			/// </summary>
+			/// <param name="direction">1 - left, 2 - up, 3 - right, 4 - down</param>
+			/// <returns>Moved point.</returns>
+			public Point Move (int direction) {
+				switch (direction) {
+					case 1 : 
+						return new Point(x - 1, y);
+					case 2 :
+						return new Point(x, y - 1);
+					case 3 : 
+						return new Point(x + 1, y);
+					case 4 : 
+						return new Point(x, y + 1);
+				}
+
+				throw new Exception("Illegal direction!");
+			}
+
+			public override string ToString () {
+				return string.Format("Point(x = {0}, y = {1})", x, y);
+			}
+
 		}
 
 		private bool[,] walls;
@@ -29,38 +52,48 @@ namespace Test2 {
 		private int width;
 		private int height;
 
-		public void Load (string filename, char separator) {
+		public void Load (string filename) {
 			string[] lines = File.ReadAllLines(filename);
 
 			height = lines.Length;
-			width = lines[0].Split(separator).Length;
+			width = lines[0].Length;
 
 			walls = new bool[height, width];
 
 			for (int i = 0; i != height; i++) {
-				int[] row = lines[i].Split(separator).Select(s => Convert.ToInt32(s)).ToArray();
-
 				for (int j = 0; j != width; j++) {
-					if (row[j] == 3) {
+					if (lines[i][j] == '3') {
 						walls[i, j] = true;
 						continue;
 					}
 
 					walls[i, j] = false;
 
-					switch (row[j]) {
-						case 1 :
+					switch (lines[i][j]) {
+						case '1' :
 							boxPos = new Point(i, j);
 							break;
-						case 2 :
+						case '2' :
 							targetPos = new Point(i, j);
 							break;
-						case 4 :
+						case '4' :
 							playerPos = new Point(i, j);
 							break;
 					}
 				}
 			}
+
+			Console.WriteLine("Width: {0}, Height: {1}\n" +
+			                  "Target: {2}, Box: {3}, Player: {4}",
+			                  width,
+			                  height,
+			                  targetPos,
+			                  boxPos,
+			                  playerPos);
+		}
+		
+		private bool IsPointLegal (Point point) {
+			return point.x >= 0 && point.y >= 0 && point.x < width && point.y < height;
 		}
 
 		public int[] Solve () {
